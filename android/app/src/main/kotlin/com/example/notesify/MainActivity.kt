0ccd7cf049f8f528936c6bb322bcf9bf8a744d1d
mcbryan1 <mcbryansolomon@yahoo.com>
2022-06-14 09:37:23 +0000

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/notesify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.notesify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/major/home/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
